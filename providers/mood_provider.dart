514fa26fa26b34840f1d7faeeab20cb6dc8292cd
ayaulymmr <125123051+ayaulymmr@users.noreply.github.com>
2026-03-14 12:49:27 +0500

    Implement MoodProvider with mood selection functionality

--- providers/mood_provider.dart
+++ providers/mood_provider.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
+import '../models/mood.dart';
 
+class MoodProvider extends ChangeNotifier {
+  Mood? _selectedMood;
+  
+  Mood? get selectedMood => _selectedMood;
+  
+  void selectMood(Mood mood) {
+    _selectedMood = mood;
+    notifyListeners();
+  }
+}
