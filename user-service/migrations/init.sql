8d19acb31b8146643307a4de6a206b6d64d5cdee
Arailym Mukazhan <230082@astanait.edu.kz>
2026-05-09 00:44:57 +0500

    Arailym

--- user-service/migrations/init.sql
+++ user-service/migrations/init.sql
@@ -14,3 +14,11 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
+
+CREATE TABLE IF NOT EXISTS refresh_tokens (
+                                              id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    token TEXT NOT NULL UNIQUE,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
