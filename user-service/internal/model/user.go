925870d33e14df1c2ba2aeb6e9ddfb4f59de8d9a
Arailym Mukazhan <153664222+araichok@users.noreply.github.com>
2026-05-08 23:30:52 +0500

    Delete user-service directory

--- user-service/internal/model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type User struct {
-	ID       string `bson:"id"`
-	Email    string `bson:"email"`
-	Password string `bson:"password"`
-	Name     string `bson:"name"`
-	Role     string `bson:"role"`
-}
