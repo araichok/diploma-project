c4b46b25d68795010068410c18df1ce7e55af761
samatovag <230026@astanait.edu.kz>
2026-05-10 18:38:13 +0500

    Guldana

--- notification-service/internal/repository/notification_repository.go
+++ notification-service/internal/repository/notification_repository.go
@@ -2,6 +2,11 @@ package repository
 
 import (
 	"database/sql"
+	"time"
+
+	"github.com/google/uuid"
+
+	"notification-service/internal/model"
 )
 
 type NotificationRepository struct {
@@ -11,3 +16,8 @@ type NotificationRepository struct {
 func NewNotificationRepository(db *sql.DB) *NotificationRepository {
 	return &NotificationRepository{db: db}
 }
+
+func (r *NotificationRepository) Create(n *model.Notification) error {
+	n.ID = uuid.New().String()
+	n.IsRead = false
+	n.CreatedAt = time.Now()
