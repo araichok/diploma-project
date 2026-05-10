cd4616d70574e44b576c3515761daf5e91068e2f
samatovag <230026@astanait.edu.kz>
2026-05-10 22:36:18 +0500

    Guldana

--- notification-service/internal/repository/notification_repository.go
+++ notification-service/internal/repository/notification_repository.go
@@ -21,3 +21,11 @@ func (r *NotificationRepository) Create(n *model.Notification) error {
 	n.ID = uuid.New().String()
 	n.IsRead = false
 	n.CreatedAt = time.Now()
+
+	query := `INSERT INTO notifications (id, user_id, message, type, is_read, created_at)
+			  VALUES ($1, $2, $3, $4, $5, $6)`
+
+	
+	)
+	return err
+}
