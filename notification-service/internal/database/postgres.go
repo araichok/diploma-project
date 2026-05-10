9ffa7d36a1fac405ff7b9f6103b03989ea2b7727
samatovag <230026@astanait.edu.kz>
2026-05-10 15:57:21 +0500

    Guldana

--- notification-service/internal/database/postgres.go
+++ notification-service/internal/database/postgres.go
@@ -8,3 +8,9 @@ import (
 
 	_ "github.com/lib/pq"
 )
+
+func Connect() *sql.DB {
+	host := getEnv("DB_HOST", "localhost")
+	port := getEnv("DB_PORT", "5432")
+	user := getEnv("DB_USER", "postgres")
+	
