685770a7cfffa2f37dccfa7980f3e3a683dea811
samatovag <230026@astanait.edu.kz>
2026-05-10 15:57:49 +0500

    Guldana

--- notification-service/internal/database/postgres.go
+++ notification-service/internal/database/postgres.go
@@ -13,4 +13,5 @@ func Connect() *sql.DB {
 	host := getEnv("DB_HOST", "localhost")
 	port := getEnv("DB_PORT", "5432")
 	user := getEnv("DB_USER", "postgres")
-	
+	password := getEnv("DB_PASSWORD", "postgres")
+	dbname := getEnv("DB_NAME", "notification_db")
