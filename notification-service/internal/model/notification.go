e4c9b0b9c41a5cfa57a0ce7c554d8f7ace3521ec
samatovag <230026@astanait.edu.kz>
2026-05-10 15:46:23 +0500

    Guldana

--- notification-service/internal/model/notification.go
+++ notification-service/internal/model/notification.go
@@ -1,7 +1,7 @@
 package model
 
-// Notification represents a notification sent to a user
 type Notification struct {
-	ID     string `json:"id"`
-	UserID string `json:"user_id"`
+	ID      string `json:"id"`
+	UserID  string `json:"user_id"`
+	Message string `json:"message"`
 }
