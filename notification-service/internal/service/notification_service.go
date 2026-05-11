86cdc86531d513ef61c593ba221316ab8a36742c
samatovag <230026@astanait.edu.kz>
2026-05-11 17:31:43 +0500

    notif service(SendNotification creates and saves a new notification

--- notification-service/internal/service/notification_service.go
+++ notification-service/internal/service/notification_service.go
@@ -1,14 +1,29 @@
 package service
 
 import (
+	"notification-service/internal/model"
 	"notification-service/internal/repository"
 )
 
-// business logic for notifications
 type NotificationService struct {
 	repo *repository.NotificationRepository
 }
 
 func NewNotificationService(repo *repository.NotificationRepository) *NotificationService {
 	return &NotificationService{repo: repo}
+}
+
+func (s *NotificationService) SendNotification(userID, message, notifType string) (*model.Notification, error) {
+	notification := &model.Notification{
+		UserID:  userID,
+		Message: message,
+		Type:    notifType,
+	}
+
+	err := s.repo.Create(notification)
+	if err != nil {
+		return nil, err
+	}
+
+	return notification, nil
 }
