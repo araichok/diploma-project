f8b7ef25292b46e881008745a445e0976e4a3836
Arailym Mukazhan <230082@astanait.edu.kz>
2026-05-12 01:55:55 +0500

    location service

--- location-service/internal/repository/location_repository.go
+++ location-service/internal/repository/location_repository.go
@@ -1,11 +1,36 @@
 package repository
 
-import "location-service/internal/model"
+import (
+	"database/sql"
 
-type LocationRepository interface {
-	Create(location *model.Location) error
-	GetByID(id string) (*model.Location, error)
-	GetAll() ([]*model.Location, error)
-	Delete(id string) error
-	Search(city string, category string, minRating float64) ([]*model.Location, error)
+	"location-service/internal/model"
+)
+
+type LocationRepository struct {
+	db *sql.DB
+}
+
+func NewLocationRepository(db *sql.DB) *LocationRepository {
+	return &LocationRepository{db: db}
+}
+
+func (r *LocationRepository) SaveIfNotExists(location model.Location) error {
+	query := `
+		INSERT INTO locations (place_id, name, type, city, lat, lon, mood)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)
+		ON CONFLICT (place_id) DO NOTHING
+	`
+
+	_, err := r.db.Exec(
+		query,
+		location.PlaceID,
+		location.Name,
+		location.Type,
+		location.City,
+		location.Lat,
+		location.Lon,
+		location.Mood,
+	)
+
+	return err
 }
