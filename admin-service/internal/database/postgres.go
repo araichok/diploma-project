a507467592577b0a89f5f5f44a0bc9688590b672
samatovag <230026@astanait.edu.kz>
2026-05-11 19:04:01 +0500

    internal db

---
+++ admin-service/internal/database/postgres.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func Connect() *sql.DB {
+	host := getEnv("DB_HOST", "localhost")
+	port := getEnv("DB_PORT", "5432")
+	user := getEnv("DB_USER", "postgres")
+	password := getEnv("DB_PASSWORD", "postgres")
+	dbname := getEnv("DB_NAME", "admin_db")
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	log.Println("connected to PostgreSQL successfully")
+	return db
+}
