2536047000eff1b85ced71ed4c955e279ce58d73
Arailym Mukazhan <230082@astanait.edu.kz>
2026-05-12 13:47:23 +0500

    location service

--- location-service/internal/handler/location_handler.go
+++ location-service/internal/handler/location_handler.go
@@ -1,1 +1,42 @@
 package handler
+
+import (
+	"context"
+
+	"location-service/internal/service"
+	locationpb "location-service/proto/locationpb"
+)
+
+type LocationGrpcHandler struct {
+	locationpb.UnimplementedLocationServiceServer
+	locationService *service.LocationService
+}
+
+func NewLocationGrpcHandler(locationService *service.LocationService) *LocationGrpcHandler {
+	return &LocationGrpcHandler{
+		locationService: locationService,
+	}
+}
+
+func (h *LocationGrpcHandler) FindSuitableLocations(
+	ctx context.Context,
+	req *locationpb.FindLocationsRequest,
+) (*locationpb.FindLocationsResponse, error) {
+
+	return h.locationService.FindSuitableLocations(
+		ctx,
+		req.Mood,
+		req.Date,
+		req.Budget,
+		req.Duration,
+		req.Location,
+	)
+}
+
+func (h *LocationGrpcHandler) GetLocationDetails(
+	ctx context.Context,
+	req *locationpb.GetLocationDetailsRequest,
+) (*locationpb.LocationDetailsResponse, error) {
+
+	return h.locationService.GetLocationDetails(ctx, req.PlaceId)
+}
