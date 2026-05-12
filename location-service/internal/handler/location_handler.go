fd9b449083719b1768e5df9d8988ffccf5cc2679
Arailym Mukazhan <230082@astanait.edu.kz>
2026-05-12 14:10:33 +0500

    location service

--- location-service/internal/handler/location_handler.go
+++ location-service/internal/handler/location_handler.go
@@ -23,14 +23,31 @@ func (h *LocationGrpcHandler) FindSuitableLocations(
 	req *locationpb.FindLocationsRequest,
 ) (*locationpb.FindLocationsResponse, error) {
 
-	return h.locationService.FindSuitableLocations(
-		ctx,
+	locations, err := h.locationService.FindSuitableLocations(
 		req.Mood,
-		req.Date,
-		req.Budget,
-		req.Duration,
 		req.Location,
 	)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []*locationpb.Location
+
+	for _, location := range locations {
+		result = append(result, &locationpb.Location{
+			PlaceId: location.PlaceID,
+			Name:    location.Name,
+			Type:    location.Type,
+			City:    location.City,
+			Lat:     location.Lat,
+			Lon:     location.Lon,
+			Mood:    location.Mood,
+		})
+	}
+
+	return &locationpb.FindLocationsResponse{
+		Locations: result,
+	}, nil
 }
 
 func (h *LocationGrpcHandler) GetLocationDetails(
@@ -38,5 +55,18 @@ func (h *LocationGrpcHandler) GetLocationDetails(
 	req *locationpb.GetLocationDetailsRequest,
 ) (*locationpb.LocationDetailsResponse, error) {
 
-	return h.locationService.GetLocationDetails(ctx, req.PlaceId)
+	details, err := h.locationService.GetLocationDetails(req.PlaceId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &locationpb.LocationDetailsResponse{
+		PlaceId:      details.PlaceID,
+		Name:         details.Name,
+		Address:      details.Address,
+		Website:      details.Website,
+		Phone:        details.Phone,
+		OpeningHours: details.OpeningHours,
+		Description:  details.Description,
+	}, nil
 }
