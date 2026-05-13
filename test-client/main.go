d15332ec881c371b5f19b115d875e05f16852029
samatovag <230026@astanait.edu.kz>
2026-05-13 23:58:27 +0500

    testclient-upd(includes routehistory,feedb serv)

--- test-client/main.go
+++ test-client/main.go
@@ -39,3 +39,26 @@ func main() {
 	fmt.Println("History ID:", historyRes.History.HistoryId)
 	fmt.Println("Status:", historyRes.History.Status)
 	fmt.Println()
+
+	feedbackConn, err := grpc.Dial("localhost:50055", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("could not connect to feedback-service: %v", err)
+	}
+	defer feedbackConn.Close()
+	feedbackClient := feedbackpb.NewFeedbackServiceClient(feedbackConn)
+
+	feedbackRes, err := feedbackClient.CreateFeedback(ctx, &feedbackpb.CreateFeedbackRequest{
+		UserId:     "user-001",
+		RouteId:    "route-101",
+		LocationId: "location-001",
+		Rating:     5,
+		Comment:    "Amazing tourist route in Astana!",
+	})
+	if err != nil {
+		log.Fatalf("CreateFeedback failed: %v", err)
+	}
+	fmt.Println("✅ Feedback created successfully")
+	fmt.Println("Feedback ID:", feedbackRes.Feedback.FeedbackId)
+	fmt.Println("Rating:", feedbackRes.Feedback.Rating)
+	fmt.Println("Comment:", feedbackRes.Feedback.Comment)
+	fmt.Println()
