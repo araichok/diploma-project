b281a656fad3e16805755c8a9b253bcc20f563a7
samatovag <230026@astanait.edu.kz>
2026-05-13 20:30:30 +0500

    connect route-history-service to notification-service via gRPC

---
+++ route-history-service/internal/client/notification_client.go
@@ -0,0 +1,41 @@
+package client
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "notification-service/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type NotificationClient struct {
+	client pb.NotificationServiceClient
+}
+
+func NewNotificationClient() *NotificationClient {
+	conn, err := grpc.Dial("localhost:50057", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("could not connect to notification-service: %v", err)
+	}
+
+	return &NotificationClient{
+		client: pb.NewNotificationServiceClient(conn),
+	}
+}
+
+func (n *NotificationClient) SendNotification(userID, message, notifType string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := n.client.SendNotification(ctx, &pb.SendNotificationRequest{
+		UserId:  userID,
+		Message: message,
+		Type:    notifType,
+	})
+	if err != nil {
+		log.Println("Error sending notification:", err)
+	}
+}
