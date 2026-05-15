11dd27249c210e722ed0ce85917dc4bf3b5fb447
Arailym Mukazhan <230082@astanait.edu.kz>
2026-05-15 22:00:32 +0500

    api gateway

--- route-generation-service/internal/config/config.go
+++ route-generation-service/internal/config/config.go
@@ -7,28 +7,30 @@ import (
 )
 
 type Config struct {
-	GRPCPort            string
-	PostgresHost        string
-	PostgresPort        string
-	PostgresUser        string
-	PostgresPassword    string
-	PostgresDB          string
-	NatsURL             string
-	LocationServiceAddr string
+	GRPCPort                string
+	PostgresHost            string
+	PostgresPort            string
+	PostgresUser            string
+	PostgresPassword        string
+	PostgresDB              string
+	NatsURL                 string
+	LocationServiceAddr     string
+	RouteHistoryServiceAddr string
 }
 
 func LoadConfig() *Config {
 	_ = godotenv.Load()
 
 	return &Config{
-		GRPCPort:            getEnv("GRPC_PORT", "50053"),
-		PostgresHost:        getEnv("POSTGRES_HOST", "localhost"),
-		PostgresPort:        getEnv("POSTGRES_PORT", "5432"),
-		PostgresUser:        getEnv("POSTGRES_USER", "postgres"),
-		PostgresPassword:    getEnv("POSTGRES_PASSWORD", "postgres"),
-		PostgresDB:          getEnv("POSTGRES_DB", "route_db"),
-		NatsURL:             getEnv("NATS_URL", "nats://localhost:4222"),
-		LocationServiceAddr: getEnv("LOCATION_SERVICE_ADDR", "localhost:50054"),
+		GRPCPort:                getEnv("GRPC_PORT", "50053"),
+		PostgresHost:            getEnv("POSTGRES_HOST", "localhost"),
+		PostgresPort:            getEnv("POSTGRES_PORT", "5432"),
+		PostgresUser:            getEnv("POSTGRES_USER", "postgres"),
+		PostgresPassword:        getEnv("POSTGRES_PASSWORD", "postgres"),
+		PostgresDB:              getEnv("POSTGRES_DB", "route_db"),
+		NatsURL:                 getEnv("NATS_URL", "nats://localhost:4222"),
+		LocationServiceAddr:     getEnv("LOCATION_SERVICE_ADDR", "localhost:50054"),
+		RouteHistoryServiceAddr: getEnv("ROUTE_HISTORY_SERVICE_ADDR", "localhost:50056"),
 	}
 }
 
