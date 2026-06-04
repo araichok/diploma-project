0a30de98a69064a659196f5837695ff770b55f3d
Arailym Mukazhan <230082@astanait.edu.kz>
2026-06-04 13:37:41 +0500

    fixes for "address"

--- location-service/migrations/init.sql
+++ location-service/migrations/init.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS locations (
                                          place_id VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255) NOT NULL,
     type VARCHAR(255),
+    address TEXT,
     city VARCHAR(255),
     lat DOUBLE PRECISION,
     lon DOUBLE PRECISION,
